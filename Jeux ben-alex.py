f295dcf80f572da13077a9ca17f93240f0ae0180
nhuot <nhuot@leseminaire.ca>
2024-11-21 16:07:49 +0000

    Debut

--- Jeux ben-alex.py
+++ Jeux ben-alex.py
@@ -1,1 +1,19 @@
 
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([500, 500])
+running = True
+position_personage = (100, 100)
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255))
+
+    pygame.draw.circle(screen, (0, 0, 255), (260, 0), 75)
+    pygame.draw.ellipse(screen, (0, 0, 255), position_personage, 75)
+    pygame.display.flip()       
+
+pygame.quit()
