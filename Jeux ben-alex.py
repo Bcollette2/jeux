eb9f8f52d06222e7800dbf4a372ac0074969572c
nhuot <nhuot@leseminaire.ca>
2024-11-24 11:45:00 -0500

    Update Jeux ben-alex.py

--- Jeux ben-alex.py
+++ Jeux ben-alex.py
@@ -4,7 +4,11 @@ pygame.init()
 
 screen = pygame.display.set_mode([500, 500])
 running = True
+personage = pygame.image.load('./IMG_3545.png').convert_alpha()
+personage = pygame.transform.scale(personage, (50, 80))
+print(personage)
 position_personage = (100, 100)
+position_cercle = (260, 0)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -12,8 +16,9 @@ while running:
 
     screen.fill((255, 255, 255))
 
-    pygame.draw.circle(screen, (0, 0, 255), (260, 0), 75)
-    pygame.draw.ellipse(screen, (0, 0, 255), position_personage, 75)
+    pygame.draw.circle(screen, (0, 0, 255), position_cercle, 75)
+    position_cercle = (position_cercle[0], position_cercle[1]+0.005)
+    screen.blit(personage, position_personage)
     pygame.display.flip()       
 
 pygame.quit()
